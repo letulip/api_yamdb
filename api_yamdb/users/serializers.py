4aec01c69eaa4299b042911faf6f54489ee19bad
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2022-04-15 17:36:53 +0300

    feat: add user registration confirmation code

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,6 +1,10 @@
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
 from rest_framework import serializers
 
 from .models import CustomUser
+from .tokens import get_token
 
 class UsersSerializer(serializers.ModelSerializer):
 
@@ -14,3 +18,24 @@ class UsersSerializer(serializers.ModelSerializer):
             'role'
         )
         model = CustomUser
+
+    def create(self, validated_data):
+        new_user = CustomUser.objects.create(**validated_data)
+        username = validated_data.pop('username')
+        email = validated_data.pop('email')
+        code = get_token.make_token(new_user)
+        # send_mail(
+        #     from_email='from@example.com',
+        #     subject=f'Hello, {username} Confirm your email',
+        #     message=f'Your confirmation code: {code}.',
+        #     recipient_list=[
+        #         email,
+        #     ],
+        #     fail_silently=False,
+        # )
+        message = (
+            f'Hello, {username} Confirm your email {email}',
+            f'Your confirmation code: {code}.',
+        )
+        print(message)
+        return new_user
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
+# from django.core.mail import send_mail
+# from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 from rest_framework import viewsets, filters
 
@@ -18,3 +20,15 @@ class UsersViewSet(viewsets.ModelViewSet):
         '$username'
     )
     lookup_field = 'username'
+
+    # def perform_create(self, serializer):
+    #     username = self.request.user.username
+    #     email = self.request.user.email
+    #     serializer.save(
+    #         username=username,
+    #         email=email
+    #     )
+    #     user = get_object_or_404(CustomUser, username=username)
+    #     code = PasswordResetTokenGenerator.make_token(user)
+    #     print(code)
+
