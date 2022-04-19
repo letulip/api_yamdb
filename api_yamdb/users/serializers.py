fa1ced59c928d24e5a4409144e4820d9c1a61a09
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2022-04-19 20:25:47 +0300

    delete: prints and commented code

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -36,34 +36,6 @@ class UsersSerializer(serializers.ModelSerializer):
         """
         return make_password(value)
 
-    # def create(self, validated_data):
-    #     print('we can create')
-    #     new_user = CustomUser.objects.create(**validated_data)
-    #     username = validated_data.pop('username')
-    #     email = validated_data.pop('email')
-    #     code = get_check_hash.make_token(new_user)
-    #     # send_mail(
-    #     #     from_email='from@example.com',
-    #     #     subject=f'Hello, {username} Confirm your email',
-    #     #     message=f'Your confirmation code: {code}.',
-    #     #     recipient_list=[
-    #     #         email,
-    #     #     ],
-    #     #     fail_silently=False,
-    #     # )
-    #     message = (
-    #         f'Hello, {username} Confirm your email {email}',
-    #         f'Your confirmation code: {code}.',
-    #     )
-    #     print(code)
-    #     return get_object_or_404(CustomUser, username=username)
-    #     # return new_user
-    #     # user = get_object_or_404(CustomUser, username=username)
-    #     response = Response(data=new_user, status=HTTP_200_OK)
-    #     print(response.__dict__)
-    #     return response
-    #     return Response(data=new_user, status=HTTP_200_OK)
-
 
 class UserSelfSerializer(UsersSerializer):
     username = serializers.CharField(
