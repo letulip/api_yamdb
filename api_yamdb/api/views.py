2c7d05f7aa6c100bf6c978be76acbab3d2c3a3aa
Konstantin <89670253660@mail.ru>
2022-04-21 21:56:08 +0300

    comments

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -153,14 +153,6 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(
             author=self.request.user,
             review=review,
-            #title=title_id,
         )
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        #if self.request.user != 
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
 
         return Response(serializer.data, status=HTTP_200_OK)
